2bf78e7b293b1985a3f0ccb759690c0a3f584195
Cyrus <26298582+dwaynemcyrus@users.noreply.github.com>
2026-01-07 23:28:09 +0100

    feat: add command test page

---
+++ app/(app)/command/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function CommandPage() {
+  return (
+    <div>
+      <h1>Command</h1>
+      <p>Test flow for the push overlay stack.</p>
+      <Link href="/projects">Go to Projects</Link>
+    </div>
+  );
+}
